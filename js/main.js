04e2b06482d2f774248057de9a06581ac1f6bc21
Debora Gidaszewski <deboragida@gmail.com>
2023-01-17 17:12:26 -0300

    v4

--- js/main.js
+++ js/main.js
@@ -1,46 +1,3 @@
-/*let cantidad = 0;
-let detalle = 0;
-let precio = 0;
-
-parseFloat(cantidad);
-parseFloat(precio);
-
-function productos (cantidad, detalle, precio) {
-    this.cantidad = cantidad;
-    this.detalle = detalle;
-    this.precio = precio;
-}
-
-const prod1 = new productos (cantidad, detalle, precio);
-
-const ventas = [prod1];
-
-for (let i = 0; i < ventas.length; i ++){
-
-    let cantidad = prompt ("Ingresá la cantidad vendida");
-    let detalle = prompt ("Ingresá el nombre del producto");
-    let precio = prompt ("Ingresá el precio del producto que figura en el catálogo");
-
-    parseFloat (cantidad);
-    parseFloat (precio);
-
-    function calcularComision (precio) {
-    return (precio * cantidad) * 0.3 
-    }
-    
-    let comisionVenta = calcularComision (precio)
-    alert ("Tu ganancia es: " + comisionVenta)
-    
-    function comisionNatura (precio) {
-    return (precio * cantidad) - (comisionVenta)
-    }
-    
-    let pagoNatura = comisionNatura (precio)
-    alert ("Debes pagarle a Natura Cosmeticos: " + pagoNatura);
-}
-
-console.log (ventas);*/
-
 const cantidad = document.querySelector("#cantidad"),
   codigo = document.querySelector("#codigo"),
   producto = document.querySelector("#producto"),
@@ -54,20 +11,19 @@ parseFloat(cantidad);
 parseFloat(precio);
 
 //Constructor del objeto
-function productos(cantidad, codigo, producto, precio) {
-  this.cantidad = cantidad;
-  this.codigo = codigo;
-  this.producto = producto;
-  this.precio = precio;
-}
-
-//Funciones comisiones
-function comision (precio) {
-    return (precio * cantidad) * 0.3 
-}
-
-function pago (precio) {
-    return (precio * cantidad) - (comision)
+class productos {
+  constructor(cantidad, codigo, producto, precio) {
+    this.cantidad = cantidad;
+    this.codigo = codigo;
+    this.producto = producto;
+    this.precio = precio;
+  }
+  calcularComision() {
+    this.comision = this.precio * this.cantidad * 0.3;
+  }
+  calcularPago() {
+    this.pago = this.precio * this.cantidad - this.comision;
+  }
 }
 
 //Funcion cargar a ventas
@@ -77,8 +33,8 @@ function cargarVentas(arr, productos) {
 
 //Funciones de LS
 function guardarLS(arr) {
-    localStorage.setItem("ventas", JSON.stringify(arr));
-  }
+  localStorage.setItem("ventas", JSON.stringify(arr));
+}
 
 //DOM
 function crearHtml(arr) {
@@ -93,25 +49,24 @@ function crearHtml(arr) {
     <td>${item.precio}</td>
     <td>${item.comision}</td>
     <td>${item.pago}</td>
-    <td><button class="btn btn-danger" id="${item.codigo}">Borrar</button></td>
+    <td><button class="btn btn-danger" id="${item.codigo}"> Borrar </button></td>
     </tr>`;
     tbody.innerHTML += html;
   }
 }
 
+/* Borrar productos NO ME FUNCIONA */
+const arrayBorrar = document.querySelectorAll("td .btn.btn-danger");
+arrayBorrar.forEach((btn)=>{
+    btn.btn-danger.addEventListener('click',()=>{
+        ventas = ventas.filter((el) => el.codigo != btn.btn-danger.id);
+        console.log(ventas);
 
-/* Agregar eventos a los botones */
- const arrayBotones = document.querySelectorAll("td .btn");
-  arrayBotones.forEach((btn)=>{
-    btn.addEventListener('click',()=>{
-      ventas = ventas.filter((el) => el.codigo != btn.id);
-      console.log(ventas);
-
-      guardarLS(ventas);
-      crearHtml(ventas);
+        guardarLS(ventas);
+        crearHtml(ventas);
     })
-  })
-
+})
+    
 crearHtml(ventas);
 
 //Listeners
@@ -121,14 +76,17 @@ formulario.addEventListener("submit", (e) => {
     cantidad.value,
     codigo.value,
     producto.value,
-    precio.value,
-    comision.value,
-    pago.value
+    precio.value
   );
+
+  nuevoProd.calcularComision();
+  nuevoProd.calcularPago();
 
   console.log(nuevoProd);
   cargarVentas(ventas, nuevoProd);
   guardarLS(ventas);
   crearHtml(ventas);
-  formulario.reset();
+
+  formulario.reset()
+
 });
